40553235e57fc49c5a514f6deb363305fea7ce29
Prathamesh Shinde <98965952+Prathamesh2911@users.noreply.github.com>
2024-11-17 18:22:21 +0530

    commented one import

--- lib/first.dart
+++ lib/first.dart
@@ -4,7 +4,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 
 import 'package:barcode_widget/barcode_widget.dart';
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
 class First extends StatefulWidget {
   const First({super.key});
